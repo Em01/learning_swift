80fbd2789243adc3252fe092de70148064086a01
Em01 <emma_williams1@hotmail.co.uk>
2015-04-23 17:24:18 +0100

    conditionals

--- loops/If.playground/section-1.swift
+++ loops/If.playground/section-1.swift
@@ -1,5 +1,26 @@
-// Playground - noun: a place where people can play
+// Conditionals
 
 import UIKit
 
-var str = "Hello, playground"
+let cards = 1...13
+
+for card in cards {
+    if card == 11 {
+        println("Jack")
+    } else if card == 12 {
+        println("Queen")
+    } else {
+        println(card)
+    }
+}
+
+let months = [1, 2, 3]
+for month in months {
+    if month == 1 {
+        println("January")
+    } else if month == 2 {
+        println("Febrauary")
+    } else {
+        println("March")
+    }
+}
