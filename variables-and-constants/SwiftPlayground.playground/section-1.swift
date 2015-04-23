3e57324f348f5e63db884daf1cb97b8207834f8e
Em01 <emma_williams1@hotmail.co.uk>
2015-04-23 11:04:21 +0100

    variables and constants

---
+++ variables-and-constants/SwiftPlayground.playground/section-1.swift
@@ -0,0 +1,15 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str : String = "Hello, playground"
+str = "Hello World"
+str
+
+var programmingLanguage: String = "Swift"
+programmingLanguage
+
+var greeting: String = "Hello" + programmingLanguage
+// implicit stype String or inferred type String
+
+var number = 11
