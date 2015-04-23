71ce5a87ecb0587c322e8d92f519808f21302584
Em01 <emma_williams1@hotmail.co.uk>
2015-04-23 15:28:47 +0100

    dictionary

---
+++ collections/Dictionary.playground/section-1.swift
@@ -0,0 +1,33 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+
+// Code     Country Name
+//or 
+// Key      Value
+//CA        Candada
+//BE        Belgium
+
+var countries = [ "CA": "Canada", "BE": "Belgium", "FR": "France" ]
+
+countries["CA"]
+
+countries["US"] = "United States"
+countries
+
+countries["US"] = "United States of America"
+
+
+
+
+let item = countries.removeValueForKey("CA")
+item
+
+let currencies = [ "US": "Dollar", "UK": "Pound", "JP": "Yen" ]
+currencies
+
+
+
+
+
