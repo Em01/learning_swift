8bea39d2a6ed1b0b08ff9068e46c3a0ef06043b0
Em01 <emma_williams1@hotmail.co.uk>
2015-04-24 12:25:47 +0100

    initial funcs

---
+++ functions/Functions.playground/section-1.swift
@@ -0,0 +1,23 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+println("Swift Functions")
+
+func calculateArea() {
+let height = 12
+let width = 10
+let area = height * width
+println("The area of the room is \(area)")
+}
+
+
+calculateArea()
+
+
+
+
+func greeting() {
+    println ("Hello")
+}
+
